e4e595908721f707788b8d94f9e658f1ecd18bc1
laqiiz <mano.junki@gmail.com>
2019-05-18 23:08:44 +0900

    fix #6 available argument format https://github.com/<user>/<repo>

--- cmd/root.go
+++ cmd/root.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"github.com/laqiiz/pkgdiagram/dependency"
 	"github.com/laqiiz/pkgdiagram/directory"
@@ -13,28 +15,70 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var targetPath string
+var outputPath string
 
 func init() {
-	rootCmd.Flags().StringVarP(&targetPath, "path", "p", ".", "target repository root path")
+	rootCmd.Flags().StringVarP(&outputPath, "file", "f", ".", "output file path")
 }
 
 var rootCmd = &cobra.Command{
 	Use:   "pgkdgm",
 	Short: "pkgdgm is a package diagram generator",
 	Long:  "pkgdgm is a tool to analyze package dependencies of Go repository and generate UML text of package diagrams.",
+	Args:  cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		targetPath := args[0]
+		dirName := filepath.Base(targetPath)
 
-		dirs, err := directory.NewWithIgnores(".git", ".idea").Do(targetPath)
+		if strings.HasPrefix(targetPath, "https://") {
+
+			prev, _ := filepath.Abs(".")
+			defer func() { _ = os.Chdir(prev) }()
+
+			userHomeDir, err := os.UserHomeDir()
+			if err != nil {
+				return err
+			}
+			tempDir := filepath.Join(userHomeDir, ".pkgdgm")
+
+			// change working directory
+			importPath := strings.TrimPrefix(targetPath, "https://")
+
+			downloadPath := filepath.Join(tempDir, importPath)
+
+			// check already imported target repository because go get is very slowly
+			if !Exists(downloadPath) {
+
+				parentDir := filepath.Clean(downloadPath + "/../")
+				if err := os.MkdirAll(parentDir, os.ModePerm); err != nil && !os.IsExist(err) {
+					return err
+				}
+
+				// cd
+				_ = os.Chdir(parentDir)
+
+				// if not existing then go get
+				output, err := exec.Command("git", "clone", targetPath).CombinedOutput()
+				if err != nil {
+					return err
+				}
+				log.Println(string(output))
+			}
+
+			// Rewrite
+			targetPath = filepath.Join(tempDir, importPath)
+		}
+
+		dirs, err := directory.NewWithIgnores(".git", ".idea", "docs", "examples").Do(targetPath)
 		if err != nil {
-			log.Println(err)
+			return err
 		}
 
 		for _, dir := range dirs {
 			log.Printf("%+v\n", dir)
 		}
 
-		parser := dependency.New(filepath.Base(targetPath))
+		parser := dependency.New(dirName)
 
 		dependencies := make(dependency.Dependencies, 0)
 		for _, v := range dirs {
@@ -53,11 +97,11 @@ var rootCmd = &cobra.Command{
 
 		file, err := os.Create("test.pu")
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 
 		if err := umlgen.New().Do(file, depends); err != nil {
-			log.Fatal(err)
+			return err
 		}
 
 		log.Println("finish")
